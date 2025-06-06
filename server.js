3fc02a738cb5374d4a2642ce94a53311f158fc36
Koray Keskin <84571672+korayzin@users.noreply.github.com>
2025-06-06 16:49:44 +0300

    Update README with custom port instructions

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  const extname = String(path.extname(filePath)).toLowerCase();
+  let contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+      contentType = 'application/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+    case '.jpeg':
+      contentType = 'image/jpeg';
+      break;
+  }
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (_err, data) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(data, 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Server Error: ' + error.code);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
